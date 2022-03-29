b3881ed791d246b37555ce854ece6c5d1a253054
samantonovich@gmail.com <samantonovich@gmail.com>
2022-03-29 18:06:09 +0300

    Added method for getting tickers

---
+++ CurrencyDotNet.RestClient/Models/Responses/Ticker.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CurrencyDotNet.RestClient.Models.Responses
+{
+    public class Ticker
+    {
+        [JsonPropertyName("symbol")]
+        public string Symbol { get; set; }
+        [JsonPropertyName("priceChange")]
+        public decimal PriceChange { get; set; }
+        [JsonPropertyName("priceChangePercent")]
+        public decimal PriceChangePercent { get; set; }
+        [JsonPropertyName("weightedAvgPrice")]
+        public decimal WeightedAveragePrice { get; set; }
+        [JsonPropertyName("lastPrice")]
+        public decimal LastPrice { get; set; }
+        [JsonPropertyName("lastQty")]
+        public decimal LastQty { get; set; }
+        [JsonPropertyName("bidPrice")]
+        public decimal BidPrice { get; set; }
+        [JsonPropertyName("askPrice")]
+        public decimal AskPrice { get; set; }
+        [JsonPropertyName("openPrice")]
+        public decimal OpenPrice { get; set; }
+        [JsonPropertyName("highPrice")]
+        public decimal HighPrice { get; set; }
+        [JsonPropertyName("lowPrice")]
+        public decimal LowPrice { get; set; }
+        [JsonPropertyName("volume")]
+        public decimal Volume { get; set; }
+        [JsonPropertyName("quoteVolume")]
+        public decimal QuoteVolume { get; set; }
+        public DateTime OpenTime {
+            get
+            {
+                return DateTime.Now;
+            } 
+            set 
+            {
+
+            } 
+        }
+        public DateTime CloseTime { get; set; }
+    }
+}
