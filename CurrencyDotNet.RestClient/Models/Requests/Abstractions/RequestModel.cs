45d27291936a23ff101395bb0a7929e56c962ffa
Vladislav Titov <capt.Hoffmann@gmail.com>
2022-03-29 21:46:00 +0300

    added base request model and key-value model for uri parameter

---
+++ CurrencyDotNet.RestClient/Models/Requests/Abstractions/RequestModel.cs
@@ -0,0 +1,26 @@
+﻿using CurrencyDotNet.Common.Models;
+using CurrencyDotNet.Common.Extensions;
+
+namespace CurrencyDotNet.RestClient.Models.Requests.Abstractions
+{
+    internal class RequestModel
+    {
+        public string GetQueryString()
+        {
+            Type myType = this.GetType();
+            var propertiesList = myType.GetProperties();
+            var parametersList = new List<string>();
+
+            foreach (var propertyInfo in propertiesList)
+            {
+                var key = propertyInfo.Name.GetUriRequestParameterKey();
+                var value = propertyInfo.GetValue(this);
+
+                var keyValue = new UriRequestParameterKeyValueModel(key, value);
+                if (!keyValue.IsEmpty()) parametersList.Add(keyValue.ToString());
+            }
+
+            return $"?{string.Join('&', parametersList)}";
+        }
+    }
+}
