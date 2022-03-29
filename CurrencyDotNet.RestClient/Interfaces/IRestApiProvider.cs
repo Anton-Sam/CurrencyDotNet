535056603d35a278fbd992373c386e0c11fcde6c
samantonovich@gmail.com <samantonovich@gmail.com>
2022-03-29 11:13:50 +0300

    Added provider for http requests

---
+++ CurrencyDotNet.RestClient/Interfaces/IRestApiProvider.cs
@@ -0,0 +1,28 @@
+﻿namespace CurrencyDotNet.RestClient.Interfaces
+{
+    /// <summary>
+    /// Provider for http requests
+    /// </summary>
+    public interface IRestApiProvider : IDisposable
+    {
+        /// <summary>
+        /// Send http GET request
+        /// </summary>
+        /// <typeparam name="T">Type of response</typeparam>
+        /// <param name="requestUri">Request uri</param>
+        /// <param name="requestModel">Request model</param>
+        /// <param name="token">Cancellation token</param>
+        /// <returns>Response object</returns>
+        public Task<T> GetRequestAsync<T>(string requestUri, IRequestModel requestModel, CancellationToken token);
+
+        /// <summary>
+        /// Send http POST request
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="requestUri">Request uri</param>
+        /// <param name="requestModel">Request model</param>
+        /// <param name="token">Cancellation token</param>
+        /// <returns>Response object</returns>
+        public Task<T> PostRequestAsync<T>(string requestUri, IRequestModel requestModel, CancellationToken token);
+    }
+}
