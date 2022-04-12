f2dddccb6a2aa2ed57e3dec781ef4938c24cf095
samantonovich@gmail.com <samantonovich@gmail.com>
2022-04-12 20:11:13 +0300

    feat: added privaterequestmodel

---
+++ CurrencyDotNet.RestClient/Models/Requests/Abstractions/PrivateRequestModel.cs
@@ -0,0 +1,21 @@
+﻿using CurrencyDotNet.Common.Extensions;
+
+namespace CurrencyDotNet.RestClient.Models.Requests.Abstractions
+{
+    public class PrivateRequestModel : RequestModel
+    {
+        public int RecvWindow { get; set; }
+        public long Timestamp { get; private set; }
+        private readonly string _apiKey;
+        public PrivateRequestModel(string apiKey) =>
+            _apiKey = apiKey;
+
+        public new string GetQueryString()
+        {
+            Timestamp = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+            var requestString = base.GetQueryString();
+            return string.Concat(requestString, "&signature=", requestString.HmacSha256(_apiKey));
+        }
+
+    }
+}
